6ecf00451a192ffc27306716a18641bbf8c2265f
Vladysalv Sharai <vladsharai3@gmail.com>
2026-04-28 16:09:13 +0200

    Second Commit

--- src/main/java/com/kodilla/kalkulator/Calkulator.java
+++ src/main/java/com/kodilla/kalkulator/Calkulator.java
@@ -1,5 +1,16 @@
 package com.kodilla.kalkulator;
 
 public class Calkulator {
+    public void someMethod() {
+        System.out.println("It works!");
+    }
+
+    public static void main(String[] args) {
+        Calkulator myCalkulator = new Calkulator();
+
+        myCalkulator.someMethod();
+
+    }
+
 
 }
